986b7e17783e9e9953025001b26a3bf65c852cc1
EtherScripts <145683801+etherscripts@users.noreply.github.com>
2023-09-24 13:40:16 +0300

    update #1

--- main.py
+++ main.py
@@ -8,24 +8,30 @@ BASE_ENDPOINT = "https://mint.fun/api/mintfun/profile/{}/offer-count/8453"
 
 
 def check_address_for_nfts(address, endpoint):
-    url = endpoint.format(address)
-    response = requests.get(url)
-    return response.json().get("totalOffers", None)
+    try:
+        url = endpoint.format(address)
+        response = requests.get(url)
+        return response.json().get("totalOffers", None)
+    except Exception as error:
+        print(f"Возникла ошибка: {error}.")
 
 
 def process_addresses(file_path):
-    with open(file_path, 'r') as file:
-        addresses = file.read().splitlines()
+    try:
+        with open(file_path, 'r') as file:
+            addresses = file.read().splitlines()
 
-    for address in addresses:
-        total_offers_eth = check_address_for_nfts(address, ETH_ENDPOINT)
-        total_offers_op = check_address_for_nfts(address, OP_ENDPOINT)
-        total_offers_base = check_address_for_nfts(address, BASE_ENDPOINT)
+        for address in addresses:
+            total_offers_eth = check_address_for_nfts(address, ETH_ENDPOINT)
+            total_offers_op = check_address_for_nfts(address, OP_ENDPOINT)
+            total_offers_base = check_address_for_nfts(address, BASE_ENDPOINT)
 
-        if any((total_offers_eth, total_offers_op, total_offers_base)):
-            print(f"| {address} | ETH TOTAL OFFERS: {total_offers_eth}"
-                  f" | OP TOTAL OFFERS: {total_offers_op}"
-                  f" | BASE TOTAL OFFERS: {total_offers_base} |")
+            if any((total_offers_eth, total_offers_op, total_offers_base)):
+                print(f"| {address} | ETH TOTAL OFFERS: {total_offers_eth}"
+                      f" | OP TOTAL OFFERS: {total_offers_op}"
+                      f" | BASE TOTAL OFFERS: {total_offers_base} |")
+    except Exception as error:
+        print(f"Возникла ошибка: {error}.")
 
 
 if __name__ == "__main__":
